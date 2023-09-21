8adba5398fd478430faf05e2ebaeeeaa32d7f23b
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2023-09-21 18:12:35 +0100

    implemented footer section This commit improves the styling of the footer section in the HTML code. It includes the following enhancements:
    - Added a decorative logo in the footer using SVG.
    - Updated the copyright notice to display 'Tech School Ltd © 2023.'
    - Improved the presentation of services and company links with better visual alignment.
    - Styled the newsletter section, making it more visually appealing with a proper input field and subscribe button.
    These changes enhance the visual appeal and usability of the footer section, providing a more polished and professional appearance to the website's footer.

---
+++ src/components/footer.php
@@ -0,0 +1,40 @@
+<footer class="flex justify-center bg-white shadow-lg p-10 border-t-2 border-gray-400">
+  <section class="footer text-base-content w-[85vw]">
+
+    <aside>
+      <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+        stroke="currentColor" class="text-emerald-700">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M4.098 19.902a3.75 3.75 0 005.304 0l6.401-6.402M6.75 21A3.75 3.75 0 013 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 003.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008z" />
+      </svg>
+      <p>Tech School Ltd &copy; 2023.<br />Providing reliable tech since 1992</p>
+    </aside>
+    <nav>
+      <header class="text-emerald-700 text-sm font-bold uppercase">Services</header>
+      <a class="link link-hover">Branding</a>
+      <a class="link link-hover">Design</a>
+      <a class="link link-hover">Marketing</a>
+      <a class="link link-hover">Advertisement</a>
+    </nav>
+    <nav>
+      <header class="text-emerald-700 text-sm font-bold uppercase">Company</header>
+      <a class="link link-hover">About us</a>
+      <a class="link link-hover">Contact</a>
+      <a class="link link-hover">Jobs</a>
+      <a class="link link-hover">Press kit</a>
+    </nav>
+    <form>
+      <header class="text-emerald-700 text-sm font-bold uppercase">Newsletter</header>
+      <fieldset class="form-control w-80">
+        <label class="label">
+          <span class="label-text">Enter your email address</span>
+        </label>
+        <div class="relative">
+          <input type="email" required placeholder="username@site.com" class="input input-bordered w-full pr-16" />
+          <button
+            class="btn bg-emerald-600 hover:bg-emerald-700 duration-500 focus:scale-90 text-white absolute top-0 right-0 rounded-l-none">Subscribe</button>
+        </div>
+      </fieldset>
+    </form>
+  </section>
+</footer>
