03348c4e64118beb01c832f37d9afed1ac0c71e2
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2023-09-27 11:17:27 +0100

    Added programmes to the home page

--- index.php
+++ index.php
@@ -19,20 +19,81 @@
     <main class="flex items-center justify-center">
       <section class="flex flex-col items-center w-[85vw]">
         <section class="my-16">
-          <h2 class="text-center mb-7 text-xl sm:text-3xl md:text-4xl font-bold text-emerald-700">Claim Your Place
+          <h2 class="text-center mb-5 text-xl sm:text-3xl md:text-4xl font-bold text-emerald-700">Claim Your Place
             in the New Global Digital Workforce</h2>
-          <p class="text-center text-base sm:text-lg md:text-xl font-medium">At <span
+          <p class="text-center mb-5 text-base sm:text-lg md:text-xl font-medium">At <span
               class="text-emerald-700">Tech</span> School,
             our programmes are designed to prepare you for a career in the world's fastest growing industries.</p>
+
+          <section class="grid grid-cols-2 lg:grid-cols-4 gap-4">
+            <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+              <img class="w-full h-48 object-cover" src="./src/assets/machine-learning.png" alt="Machine Learning">
+              <div class="p-6">
+                <h3 class="text:base sm:text-lg font-bold text-emerald-700 mb-2">Machine Learning</h3>
+                <p class="text-gray-700 hidden md:block text-base mb-3">Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Quisquam voluptatum voluptatibus, quod, quae, quia quibusdam dolorum autem nemo
+                  iusto ipsam doloremque. </p>
+                <p class="text-gray-700 block md:hidden text-xs mb-3">Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.</p>
+                <a href="#"
+                  class="inline-block text-sm sm:text-base bg-emerald-700 text-white px-4 py-2 rounded-lg hover:bg-emerald-800 duration-500">Learn
+                  More</a>
+              </div>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+              <img class="w-full h-48 object-cover" src="./src/assets/software-engineering.png"
+                alt="Software Engineering">
+              <div class="p-6">
+                <h3 class="text:base sm:text-lg font-bold text-emerald-700 mb-2">Software Engineering</h3>
+                <p class="text-gray-700 hidden md:block text-base mb-3">Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Quisquam voluptatum voluptatibus, quod, quae, quia quibusdam dolorum autem nemo
+                  iusto ipsam doloremque. </p>
+                <p class="text-gray-700 block md:hidden text-xs mb-3">Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.</p>
+                <a href="#"
+                  class="inline-block text-sm sm:text-base bg-emerald-700 text-white px-4 py-2 rounded-lg hover:bg-emerald-800 duration-500">Learn
+                  More</a>
+              </div>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+              <img class="w-full h-48 object-cover" src="./src/assets/data-analytics.png" alt="Data Analytics">
+              <div class="p-6">
+                <h3 class="text:base sm:text-lg font-bold text-emerald-700 mb-2">Data Analytics</h3>
+                <p class="text-gray-700 hidden md:block text-base mb-3">Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Quisquam voluptatum voluptatibus, quod, quae, quia quibusdam dolorum autem nemo
+                  iusto ipsam doloremque. </p>
+                <p class="text-gray-700 block md:hidden text-xs mb-3">Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.</p>
+                <a href="#"
+                  class="inline-block text-sm sm:text-base bg-emerald-700 text-white px-4 py-2 rounded-lg hover:bg-emerald-800 duration-500">Learn
+                  More</a>
+              </div>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+              <img class="w-full h-48 object-cover" src="./src/assets/artificial-intelligence.png"
+                alt="Artificial Intelligence">
+              <div class="p-6">
+                <h3 class="text:base sm:text-lg font-bold text-emerald-700 mb-2">Artificial Intelligence</h3>
+                <p class="text-gray-700 hidden md:block text-base mb-3">Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Quisquam voluptatum voluptatibus, quod, quae, quia quibusdam dolorum autem nemo
+                  iusto ipsam doloremque. </p>
+                <p class="text-gray-700 block md:hidden text-xs mb-3">Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.</p>
+                <a href="#"
+                  class="inline-block text-sm sm:text-base bg-emerald-700 text-white px-4 py-2 rounded-lg hover:bg-emerald-800 duration-500">Learn
+                  More</a>
+              </div>
+            </div>
+          </section>
         </section>
 
         <!-- what we do -->
         <section class="flex justify-between items-center mb-16">
-          <div class=" mr-10 ">
+          <div class="md:mr-10">
             <h2 class="text-center mb-7 text-xl sm:text-3xl font-bold text-emerald-700">
               What we do?
             </h2>
-            <img class="rounded-3xl block mb-3 md:hidden" width="100%" src="./src\assets\operator.png" alt="">
+            <img class="rounded-3xl block mb-3 md:hidden" width="100%" src="./src\assets\operator.png" alt="operator">
 
             <p class="text-base lg:text-lg flex text-justify">Lorem ipsum dolor sit amet consectetur
               adipisicing elit. Fugiat odit tempore error maxime delectus accusamus enim, eum, facilis ipsa sint
@@ -41,7 +102,7 @@
               reprehenderit minima! Quibusdam, quasi sit vitae aliquid debitis dolorum quam delectus, quae et mollitia
               explicabo nostrum dignissimos voluptate, ab tenetur commodi ex? Minima quasi qui quas.</p>
           </div>
-          <img class="rounded-3xl hidden md:block" width="40%" src="./src\assets\operator.png" alt="">
+          <img class="rounded-3xl hidden md:block" width="40%" src="./src\assets\operator.png" alt="operator">
         </section>
 
         <!-- About community -->
@@ -51,8 +112,6 @@
             adipisicing elit. Eius fuga illum vitae molestiae suscipit, repellendus a animi repudiandae temporibus cum
             porro fugiat nam ipsam earum architecto nulla! Totam, quidem? Esse.</p>
           <section class="mb-16 grid md:grid-cols-2 lg:grid-cols-3 gap-3">
-
-
             <section class="flex shadow-xl bg-white py-6 pr-6 pl-3 rounded-lg">
               <span class="text-emerald-700 mr-2 mt-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
--- src/components/hero.php
+++ src/components/hero.php
@@ -19,6 +19,15 @@
       <p class="text-p text-base sm:text-lg font-bold text-emerald-400">Gain the digital career-ready skills to
         transform your future
       </p>
+      <a href="./src/pages/courses.php"
+        class="flex items-center mt-5 py-1.5 px-4 space-x-2 border hover:shadow-lg hover:bg-emerald-700 duration-500 border-emerald-700 w-fit font-medium rounded-full text-base sm:text-lg"><span>Get
+          Started</span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+          class="w-4 h-4">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+        </svg>
+
+      </a>
     </div>
   </div>
 </section>
