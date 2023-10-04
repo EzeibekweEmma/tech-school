48f2ed426fabce07a5be0fcc6d056f407d44fd52
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2023-10-04 01:13:44 +0100

    Added Dashboard Page with User Profile Data Retrieval
    Included PHP code to retrieve user profile data from the database based on the session's student ID.
    Designed and implemented the Dashboard Page layout with user information display.
    Created sections for user profile picture, name, email, phone, and course details.
    Included conditional rendering of profile pictures, showing a placeholder if no image is available.
    Linked to the edit-dashboard.php page to allow users to edit their profiles.
    Incorporated styles and responsive design for a visually appealing dashboard.
    Organized the code for header, alert messages, and footer into separate components for better maintainability.

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,30 @@
 <?php
+include './src/auth/config-db.php';
 session_start();
-if (!isset($_SESSION['student_id'])) {
+$student_id = $_SESSION['student_id'];
+
+if (!isset($student_id)) {
   header('Location: ./src/auth/login.php');
   exit();
 }
+
+$sql = "SELECT * FROM students WHERE id = $student_id";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows === 1) {
+  $student = $result->fetch_assoc();
+
+  // Access user data
+  $full_name = $student['full_name'];
+  $email = $student['email'];
+  $phone = $student['phone'];
+  $profile_picture = $student['profile_picture'];
+}
+
+$conn->close();
 ?>
+
 <!doctype html>
 <html lang="en">
 
@@ -21,19 +41,74 @@ if (!isset($_SESSION['student_id'])) {
   <div class="flex flex-col justify-between min-h-screen">
     <!-- header -->
     <?php include "./src/components/header.php"; ?>
+    <!-- alert message -->
+    <?php include "./src/components/alert-message.php"; ?>
+    <!-- hero -->
+    <div class="bg-emerald-700 h-60 w-full relative">
+      <div class="flex flex-col items-center justify-center h-full">
+        <h1 class="text-white text-3xl md:text-5xl font-bold">Dashboard</h1>
+      </div>
+
+      <div class="absolute bottom-2 right-2">
+        <a href="./edit-dashboard.php"
+          class="bg-white hover:bg-white/70 hover:rotate-[360deg] duration-700 text-emerald-700 p-2 rounded-full font-medium flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+          </svg></a>
+      </div>
+    </div>
 
     <!-- main -->
-    <main class="flex items-center justify-center relative">
-      <section class="flex flex-col items-center w-[85vw]">
-
-        <?php
-        echo 'Welcome, ' . $_SESSION['student_full_name'] . '!';
-        var_dump($_SESSION);
-        ?>
-
-        <!-- alert message -->
-        <?php include "./src/components/alert-message.php"; ?>
-      </section>
+    <main class="flex flex-col items-center justify-center relative">
+      <section class="flex flex-col w-[85vw]">
+        <!-- profile picture and name -->
+        <section
+          class="flex flex-col md:flex-row justify-center md:justify-start items-center md:items-end -mt-16 md:-mt-24 duration-700 space-x-5 mb-10">
+          <div
+            class="w-48 md:w-60 h-48 md:h-60 border-4 duration-700 border-white rounded-full bg-white text-emerald-800">
+            <?php
+            if (empty($profile_picture)) {
+              echo '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full p-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>';
+            } else {
+              // TODO: fix the image path
+              echo '<img src="../src/src/' . $_SESSION['student_profile_picture'] . '" alt="profile picture" class="w-full h-full rounded-full object-cover object-center" />';
+            }
+            ?>
+          </div>
+          <div class="flex flex-col mb-1 md:mb-3">
+            <span class="text-2xl md:text-4xl font-bold mb-1 md:mb-3">
+              <?php echo $full_name ?>
+            </span>
+            <span class="text-base font-medium"><?php echo $email ?></span>
+            <span class="text-base font-medium"><?php echo $phone ?></span>
+            <div class="flex space-x-2 text-gray-500 font-medium text-sm">
+              <span>0 Courses Enrolled</span>
+              <span>-</span>
+              <span>0 Courses Completed</span>
+            </div>
+          </div>
+        </section>
+        <!-- Courses -->
+        <section class="flex justify-center w-[85vw] min-h-[50vh] mb-10">
+          <section class="flex flex-col md:flex-row justify-around duration-700">
+            <div class="w-[80vw] md:w-[40vw] rounded-lg border-2 bg-white p-2 min-h-[20vh] duration-700">
+              <h2 class="underline font-medium text-center">Courses Enrolled</h2>
+              <div class="w-full h-full flex justify-center items-center">
+                <p class="font-medium text-gray-400">No courses enrolled yet.</p>
+              </div>
+            </div>
+            <div class="w-[80vw] md:w-[40vw] rounded-lg border-2 bg-white p-2 min-h-[20vh] duration-700">
+              <h2 class="underline font-medium text-center">Courses completed</h2>
+              <div class="w-full h-full flex justify-center items-center">
+                <p class="font-medium text-gray-400">No courses Complete yet.</p>
+              </div>
+            </div>
+          </section>
+        </section>
     </main>
     <!-- footer -->
     <?php include "./src/components/footer.php"; ?>
--- src/components/header.php
+++ src/components/header.php
@@ -1,14 +1,10 @@
 <header class="flex justify-center bg-white sticky top-0 shadow-lg z-20">
   <section class="flex justify-between items-center h-16 w-[85vw]">
     <a href="/" class="flex">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="w-9 h-9 text-emerald-700">
-        <path stroke-linecap="round" stroke-linejoin="round"
-          d="M4.098 19.902a3.75 3.75 0 005.304 0l6.401-6.402M6.75 21A3.75 3.75 0 013 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 003.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008z" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9 text-emerald-700">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.098 19.902a3.75 3.75 0 005.304 0l6.401-6.402M6.75 21A3.75 3.75 0 013 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 003.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008z" />
       </svg>
-      <h2 class="text-2xl font-semibold font-mono border-emerald-700 border-2"><span
-          class="text-white bg-emerald-700 p-1 pt-0.5">Tech</span><span
-          class="text-lg font-sans text-emerald-700 px-0.5">School</span>
+      <h2 class="text-2xl font-semibold font-mono border-emerald-700 border-2"><span class="text-white bg-emerald-700 p-1 pt-0.5">Tech</span><span class="text-lg font-sans text-emerald-700 px-0.5">School</span>
       </h2>
     </a>
 
@@ -21,18 +17,15 @@
             <div class="h-1 w-full bg-emerald-700 duration-500 -translate-x-24 group-hover:translate-x-0"></div>
           </a>
 
-          <a href="./courses.php" class="group w-fit h-9 overflow-x-hidden"><span
-              class="w-full h-full px-1.5 py-1">Courses</span>
+          <a href="./courses.php" class="group w-fit h-9 overflow-x-hidden"><span class="w-full h-full px-1.5 py-1">Courses</span>
             <div class="h-1 w-full bg-emerald-700 duration-500 -translate-x-24 group-hover:translate-x-0"></div>
           </a>
 
-          <a href="./facilities.php" class="group w-fit h-9 overflow-x-hidden"><span
-              class="w-full h-full px-1.5 py-1">Facilities</span>
+          <a href="./facilities.php" class="group w-fit h-9 overflow-x-hidden"><span class="w-full h-full px-1.5 py-1">Facilities</span>
             <div class="h-1 w-full bg-emerald-700 duration-500 -translate-x-24 group-hover:translate-x-0"></div>
           </a>
 
-          <a href="./about.php" class="group w-fit h-9 overflow-x-hidden"><span
-              class="w-full h-full px-1.5 py-1">About&nbsp;Us</span>
+          <a href="./about.php" class="group w-fit h-9 overflow-x-hidden"><span class="w-full h-full px-1.5 py-1">About&nbsp;Us</span>
             <div class="h-1 w-full bg-emerald-700 duration-500 -translate-x-24 group-hover:translate-x-0"></div>
           </a>
 
@@ -41,6 +34,13 @@
         <!-- TODO: fix opacity -->
         <!-- TODO: change the src to the actual profile picture -->
       </nav>
+      <!-- if (empty($profile_picture)) {
+      echo '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full p-5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+      </svg>';
+      } else {
+      echo '<img src="../src/src/' . $_SESSION['student_profile_picture'] . '" alt="profile picture" class="w-full h-full rounded-full object-cover object-center" />';
+      } -->
       <?php
       if (isset($_SESSION['student_id'])) {
         print '<div class="relative">
@@ -48,10 +48,14 @@
           <input type="checkbox" id="profileBtn-toggle" hidden>
           <div
             class="flex justify-center rounded-full lg:rounded-lg duration-700 hover:bg-emerald-700 hover:text-white cursor-pointer items-center border-2 border-emerald-700 space-x-1 lg:px-1.5 lg:py-1">
-            <div class="w-10 h-10">
-              <img src="./src/src/' . $_SESSION['student_profile_picture'] . '" alt="profile picture"
-                class="w-full h-full rounded-full object-cover object-center" />
-            </div>
+            <div class="w-10 h-10">';
+        echo empty($_SESSION['student_profile_picture']) ? '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="w-full h-full">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+              </svg>' : ' <img src="./src/src/' . $_SESSION['student_profile_picture'] . '" alt="profile picture"
+                class="w-full h-full rounded-full object-cover object-center" />';
+        print '</div>
             <span class="hidden lg:block max-w-[11rem] truncate"> ' . $_SESSION['student_full_name'] . '</span>
           </div>
         </label>
@@ -95,16 +99,13 @@
         <input type="checkbox" id="checkbox-toggle" />
 
         <!-- hamburger icon -->
-        <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32"
-          viewBox="0 0 512 512">
+        <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
           <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
         </svg>
 
         <!-- close icon -->
-        <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32"
-          viewBox="0 0 512 512">
-          <polygon
-            points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+        <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
+          <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
         </svg>
 
       </label>
@@ -119,18 +120,15 @@
         <div class="h-1 w-full bg-white duration-500 -translate-x-24 group-hover:translate-x-0"></div>
       </a>
 
-      <a href="./courses.php" class="group w-fit h-9 overflow-x-hidden"><span
-          class="w-full h-full px-1.5 py-1">Courses</span>
+      <a href="./courses.php" class="group w-fit h-9 overflow-x-hidden"><span class="w-full h-full px-1.5 py-1">Courses</span>
         <div class="h-1 w-full bg-white duration-500 -translate-x-24 group-hover:translate-x-0"></div>
       </a>
 
-      <a href="./facilities.php" class="group w-fit h-9 overflow-x-hidden"><span
-          class="w-full h-full px-1.5 py-1">Facilities</span>
+      <a href="./facilities.php" class="group w-fit h-9 overflow-x-hidden"><span class="w-full h-full px-1.5 py-1">Facilities</span>
         <div class="h-1 w-full bg-white duration-500 -translate-x-24 group-hover:translate-x-0"></div>
       </a>
 
-      <a href="./about.php" class="group w-fit h-9 overflow-x-hidden"><span
-          class="w-full h-full px-1.5 py-1">About&nbsp;Us</span>
+      <a href="./about.php" class="group w-fit h-9 overflow-x-hidden"><span class="w-full h-full px-1.5 py-1">About&nbsp;Us</span>
         <div class="h-1 w-full bg-white duration-500 -translate-x-24 group-hover:translate-x-0"></div>
       </a>
     </div>
